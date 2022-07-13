a341b7f8c9932ade75fb1d71dfb318c29a079a5c
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-07-13 21:27:42 +0200

    Update `main.js`

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 window.addEventListener('load', () => {
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
+    const list_el = document.querySelector("#tasks");
+    
+    form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const task = input.value;
+
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
