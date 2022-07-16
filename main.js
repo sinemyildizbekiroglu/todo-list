1a66ce3d1da11cdcebeb02bf3cec63c11208228f
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-07-16 20:17:02 +0200

    Update `main.js`

--- main.js
+++ main.js
@@ -25,4 +25,8 @@ window.addEventListener('load', () => {
         task_content_el.appendChild(task_input_el);
 
 		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
+        task_actions_el.classList.add('actions');
+        
+        const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Edit';
