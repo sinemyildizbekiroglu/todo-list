e1180983b8df0a40500376946d89031a727edd93
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-07-21 09:23:54 +0200

    Update `main.js`

--- main.js
+++ main.js
@@ -41,5 +41,18 @@ window.addEventListener("load", () => {
     task_el.appendChild(task_actions_el)
 
     list_el.appendChild(task_el)
+
+    input.value = ""
+
+    task_edit_el.addEventListener("click", (e) => {
+      if (task_edit_el.innerText.toLowerCase() == "edit") {
+        task_edit_el.innerText = "Save"
+        task_input_el.removeAttribute("readonly")
+        task_input_el.focus()
+      } else {
+        task_edit_el.innerText = "Edit"
+        task_input_el.setAttribute("readonly", "readonly")
+      }
+    })
   })
 })
